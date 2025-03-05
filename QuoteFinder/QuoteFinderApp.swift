d74aeb98af4ee5ed2f7b2394b0665d29f3d1bf62
lcs-nhwang <sjnhwang@lcs.on.ca>
2025-03-04 21:52:34 -0500

    Initial Commit

---
+++ QuoteFinder/QuoteFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteFinderApp.swift
+//  QuoteFinder
+//
+//  Created by Nicholas Hwang on 4/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuoteFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
